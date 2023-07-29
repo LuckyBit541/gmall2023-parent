85f23c0a5739055d559356a0f7ce10537ac0c07c
POLARBEAR\Bert <x-bit@qq.com>
2023-07-29 14:00:21 +0800

    commit

--- gmall2023-realtime/src/main/java/org/lxk/gmall/realtime/app/dim/DimApp.java
+++ gmall2023-realtime/src/main/java/org/lxk/gmall/realtime/app/dim/DimApp.java
@@ -2,11 +2,18 @@ package org.lxk.gmall.realtime.app.dim;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.lxk.gmall.realtime.app.BaseApp;
+import org.lxk.gmall.realtime.bean.TableProcess;
 import org.lxk.gmall.realtime.common.GmallConstant;
 
 @Slf4j
@@ -18,11 +25,59 @@ public class DimApp extends BaseApp {
 
     @Override
     protected void handle(StreamExecutionEnvironment executionEnvironment, DataStreamSource<String> stream) {
-        etl(stream);
+        //todo 1 对数据进行清洗
+        SingleOutputStreamOperator<String> etledStream = etl(stream);
+        //todo 2 cdc 读取配置文件
+        SingleOutputStreamOperator<TableProcess> tp = readTableProcess(executionEnvironment);
+        creatHbashTable(tp);
     }
 
-    private void etl(DataStreamSource<String> stream) {
-        stream
+    private void creatHbashTable(SingleOutputStreamOperator<TableProcess> tp) {
+
+        return tp.map(new MapFunction<TableProcess, TableProcess>() {
+
+            @Override
+            public TableProcess map(TableProcess tableProcess) throws Exception {
+                return null;
+            }
+        })
+    }
+
+    private SingleOutputStreamOperator<TableProcess> readTableProcess(StreamExecutionEnvironment executionEnvironment) {
+        MySqlSource<String> mysqlSource = MySqlSource.<String>builder()
+                .hostname(GmallConstant.MYSQL_HOSTNAME)
+                .port(GmallConstant.MYSQL_PORT)
+                .databaseList(GmallConstant.CONFIG_DATABASE + ".table_process")
+                .username(GmallConstant.MYSQL_USERNAME)
+                .password(GmallConstant.MYSQ_PASSWD)
+                .startupOptions(StartupOptions.initial())
+                .deserializer(new JsonDebeziumDeserializationSchema())
+                .build();
+        return executionEnvironment.fromSource(mysqlSource, WatermarkStrategy.noWatermarks(), "configSource")
+                .map(new MapFunction<String, TableProcess>() {
+                    @Override
+                    public TableProcess map(String value) throws Exception {
+                        JSONObject jsonObject = JSON.parseObject(value);
+                        String op = jsonObject.getString("op");
+                        TableProcess tp;
+                        if ("d".equals(op)){
+                             tp= jsonObject.getObject("before", TableProcess.class);
+
+                        }else {
+                            tp=jsonObject.getObject("after", TableProcess.class);
+
+                        }
+                        tp.setOp(op);
+
+                        return null;
+                    }
+                })
+                .filter(tp->"dim".equals(tp.getSinkType()));
+
+    }
+
+    private SingleOutputStreamOperator<String> etl(DataStreamSource<String> stream) {
+        return stream
                 .filter(new FilterFunction<String>() {
                     @Override
                     public boolean filter(String value) throws Exception {
@@ -45,7 +100,6 @@ public class DimApp extends BaseApp {
                         }
 
                     }
-                })
-                .print();
+                });
     }
 }
--- gmall2023-realtime/src/main/java/org/lxk/gmall/realtime/common/GmallConstant.java
+++ gmall2023-realtime/src/main/java/org/lxk/gmall/realtime/common/GmallConstant.java
@@ -4,4 +4,9 @@ public class GmallConstant {
     public static final String KAFAK_BROCKERS = "hadoop162:9092,hadoop163:9092,hadoop164:9092";
 
     public static final String TOP_ODS_DB = "ods_db";
+    public static final String MYSQL_HOSTNAME = "hadoop162";
+    public static final int MYSQL_PORT = 3306;
+    public static final String CONFIG_DATABASE = "gmall2023_config";
+    public static final String MYSQL_USERNAME = "root";
+    public static final String MYSQ_PASSWD = "root";
 }
