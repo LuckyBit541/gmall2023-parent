9e92b18a2101839a670b2733a4746b77817e9ef2
eric <x-bit@qq.com>
2023-08-15 08:29:39 +0800

    commit 08-15 08:29 mac gmall-suger

---
+++ gmall-suger/src/main/java/com/example/gmallsuger/GmallSugerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gmallsuger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallSugerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallSugerApplication.class, args);
+    }
+
+}
---
+++ gmall-suger/src/test/java/com/example/gmallsuger/GmallSugerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gmallsuger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallSugerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
